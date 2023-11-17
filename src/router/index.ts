5c399af507ae393844dd7fe4165fcce61e945eeb
Albarra Naufala <albarraerdanto@gmail.com>
2023-11-17 13:17:11 +0700

    feat: added register page

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue')
     },
     {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
       path: '/components/buttons',
       name: 'components.buttons',
       component: () => import('../views/components/ButtonsView.vue')
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import ButtonPrimary from '@/components/buttons/ButtonPrimary.vue'
+import CardBordered from '@/components/cards/CardBordered.vue'
+import InputLabel from '@/components/form/InputLabel.vue'
+import InputText from '@/components/form/InputText.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function register() {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full min-h-screen p-6">
+    <CardBordered class="w-full max-w-sm">
+      <form @submit.prevent="register">
+        <h1 class="text-lg font-semibold">Register</h1>
+        <div class="mt-6">
+          <InputLabel>Name</InputLabel>
+          <InputText required class="w-full mt-2" />
+        </div>
+        <div class="mt-6">
+          <InputLabel>Email</InputLabel>
+          <InputText type="email" required class="w-full mt-2" />
+        </div>
+        <div class="mt-6">
+          <InputLabel>Password</InputLabel>
+          <InputText type="password" required class="w-full mt-2" />
+        </div>
+        <div class="mt-6">
+          <InputLabel>Password Confirmation</InputLabel>
+          <InputText type="password" required class="w-full mt-2" />
+        </div>
+        <ButtonPrimary type="submit" class="w-full mt-6"> Register </ButtonPrimary>
+        <ButtonPrimary to="/login" variant="outline" class="w-full mt-2 text-center">
+          Login
+        </ButtonPrimary>
+      </form>
+    </CardBordered>
+  </div>
+</template>
