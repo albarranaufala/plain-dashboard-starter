e1bd9f0de3474711c0ffc2af5987e36d76333520
Albarra Naufala <albarraerdanto@gmail.com>
2023-11-17 09:01:41 +0700

    feat: added modal size

--- src/components/modal/ModalWrapper.vue
+++ src/components/modal/ModalWrapper.vue
@@ -2,6 +2,15 @@
 import { ref } from 'vue'
 import IconX from '../icons/IconX.vue'
 
+withDefaults(
+  defineProps<{
+    size?: 'small' | 'medium' | 'large'
+  }>(),
+  {
+    size: 'medium'
+  }
+)
+
 const isOpen = ref(false)
 
 function openModal() {
@@ -16,9 +25,17 @@ function closeModal() {
 <template>
   <slot name="trigger" :openModal="openModal" />
   <Teleport v-if="isOpen" to="body">
-    <div class="fixed inset-0 z-40 flex items-center h-screen bg-black/50" @click="closeModal">
+    <div class="fixed inset-0 z-40 flex items-center h-screen bg-black/25" @click="closeModal">
       <div class="w-full max-h-screen p-6 overflow-auto">
-        <div class="relative max-w-xl p-6 mx-auto bg-white border" @click.stop>
+        <div
+          class="relative max-w-xl p-6 mx-auto bg-white"
+          :class="{
+            'max-w-md': size === 'small',
+            'max-w-xl': size === 'medium',
+            'max-w-3xl': size === 'large'
+          }"
+          @click.stop
+        >
           <button class="absolute top-0 right-0 p-3" @click="closeModal">
             <IconX class="w-6 h-6 text-gray-500" />
           </button>
--- src/views/components/ModalView.vue
+++ src/views/components/ModalView.vue
@@ -39,6 +39,44 @@ import ButtonSecondary from '@/components/buttons/ButtonSecondary.vue'
           </template>
         </ModalWrapper>
       </CardBordered>
+      <CardBordered class="mt-6">
+        <h1 class="mb-4 text-lg font-semibold">Large Modal</h1>
+
+        <ModalWrapper size="large">
+          <template #trigger="{ openModal }">
+            <ButtonPrimary @click="openModal"> Open Modal </ButtonPrimary>
+          </template>
+
+          <template #default="{ closeModal }">
+            <p class="font-bold">Modal Header</p>
+            <p class="mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit ex veniam, error
+              earum quod neque vel magnam nam repellendus explicabo officiis nemo animi architecto
+              nobis laudantium enim iusto voluptatibus.
+            </p>
+            <ButtonSecondary class="mt-4" @click="closeModal"> Cancel </ButtonSecondary>
+          </template>
+        </ModalWrapper>
+      </CardBordered>
+      <CardBordered class="mt-6">
+        <h1 class="mb-4 text-lg font-semibold">Small Modal</h1>
+
+        <ModalWrapper size="small">
+          <template #trigger="{ openModal }">
+            <ButtonPrimary @click="openModal"> Open Modal </ButtonPrimary>
+          </template>
+
+          <template #default="{ closeModal }">
+            <p class="font-bold">Modal Header</p>
+            <p class="mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit ex veniam, error
+              earum quod neque vel magnam nam repellendus explicabo officiis nemo animi architecto
+              nobis laudantium enim iusto voluptatibus.
+            </p>
+            <ButtonSecondary class="mt-4" @click="closeModal"> Cancel </ButtonSecondary>
+          </template>
+        </ModalWrapper>
+      </CardBordered>
     </DashboardContent>
   </DashboardLayout>
 </template>
